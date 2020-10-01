14fe784c542d5cd4c01e4bcf22b828b4192a4caa
bremxx <petr.kirienko@gmail.com>
2020-10-01 21:30:01 +0300

    добавил функцию наполнения блока .pictures фотографиями из сгенерированного массива

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 "use strict";
 
+const PHOTOS_NUM_MAX = 25;
 const LIKES_MIN = 15;
 const LIKES_MAX = 200;
+const pictures = document.querySelector(`.pictures`);
+const pictureTemplate = document
+  .querySelector(`#picture`)
+  .content.querySelector(`.picture`);
+
 const messages = [
   `Всё отлично!`,
   `В целом всё неплохо. Но не всё.`,
@@ -65,7 +71,7 @@ function getComment() {
 function getPhotos() {
   let newDescr = [];
 
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < PHOTOS_NUM_MAX; i++) {
     const newDescrItem = {};
 
     newDescrItem.url = `photos/${i + 1}.jpg`;
@@ -82,8 +88,27 @@ function getPhotos() {
 
     newDescr[i] = newDescrItem;
   }
-
   return shuffleArr(newDescr);
 }
 
 const photos = getPhotos();
+
+// Наполнение блока фотографиями из массива
+function insertPhotoElements(arr) {
+  // Функция наполнения темплейта
+  function getPhotoElement(obj) {
+    const newPicture = pictureTemplate.cloneNode(true);
+
+    newPicture.querySelector(`.picture__img`).src = obj.url;
+    newPicture.querySelector(`.picture__likes`).textContent = obj.likes;
+    newPicture.querySelector(`.picture__comments`).textContent = obj.comments.length;
+
+    return newPicture;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    pictures.appendChild(getPhotoElement(arr[i]));
+  }
+}
+
+const photoElements = insertPhotoElements(photos);
