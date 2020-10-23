2d4a8ac730b748049beb58603f009ea3fa5c9e99
bremxx <petr.kirienko@gmail.com>
2020-10-23 16:43:32 +0300

    правки модуля bigPicture

--- js/big-picture.js
+++ js/big-picture.js
@@ -11,82 +11,78 @@
   const bigPictureDescription = bigPicture.querySelector(`.social__caption`);
   const bigPictureCloseBtn = bigPicture.querySelector(`.big-picture__cancel`);
 
+  // Функция отображения окна с полноэкранной фотографией
+  function showBigPicture(currentImg) {
+    window.util.showModalWindow(bigPicture);
+    bigPictureImg.src = currentImg.url;
+    bigPictureLikesCount.textContent = currentImg.likes;
+    bigPictureCommentsCount.textContent = currentImg.comments.length;
+    bigPictureDescription.textContent = currentImg.description;
+    bigPicture.querySelector(`.social__comment-count`).classList.add(`hidden`);
+    bigPicture.querySelector(`.comments-loader`).classList.add(`hidden`);
+    insertBigPicComment(currentImg.comments);
+    // Обработчик закрытия окна по по нажатию Esc
+    document.addEventListener(`keydown`, onBigPictureEscPress);
+    // Обработчик закрытия окна по клику вне окна
+    bigPicture.addEventListener(`click`, bigPictureCloseHandler);
+    // Обработчик закрытия окна по кнопке "X"
+    bigPictureCloseBtn.addEventListener(`click`, onBigPictureCloseBtnPress);
+  }
+
+  // Функция наполнения комментария для полноэкранного фото
+  function getBigPicComment(comment) {
+    const newBigPicComment = bigPictureComment.cloneNode(true);
+    newBigPicComment.querySelector(`.social__picture`).src = comment.avatar;
+    newBigPicComment.querySelector(`.social__picture`).alt = comment.name;
+    newBigPicComment.querySelector(`.social__text`).textContent = comment.message;
+    return newBigPicComment;
+  }
+
+  // Наполнение комментариев из массива для полноэкранного фото
+  function insertBigPicComment(comments) {
+    removeBigPicComments();
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < comments.length; i++) {
+      fragment.appendChild(getBigPicComment(comments[i]));
+    }
+    return bigPictureComments.appendChild(fragment);
+  }
+
+  // Функцуия очистки комментариев для bigPicture
+  function removeBigPicComments() {
+    bigPictureComments.innerHTML = ``;
+  }
+
+  // Функция закрытия окна по кнопке Х
+  function onBigPictureCloseBtnPress() {
+    closeBigPicture();
+  }
+
+  // Функция закрытия окна по нажатию Esc
+  function onBigPictureEscPress(evt) {
+    if (evt.key === `Escape`) {
+      evt.preventDefault();
+      closeBigPicture();
+    }
+  }
+
+  // Функция закрытия окна по клику вне окна
+  function bigPictureCloseHandler(evt) {
+    if (!evt.target.closest(`.big-picture__preview`)) {
+      closeBigPicture();
+    }
+  }
+
+  // Функция закрытия окна полноэкранной фотографии
+  function closeBigPicture() {
+    window.util.hideModalWindow(bigPicture);
+    document.removeEventListener(`keydown`, onBigPictureEscPress);
+    bigPicture.removeEventListener(`click`, bigPictureCloseHandler);
+    bigPictureCloseBtn.removeEventListener(`click`, onBigPictureCloseBtnPress);
+  }
+
+
   window.bigPicture = {
-    // Функция наполнения комментария для полноэкранного фото
-    getBigPicComment(comment) {
-      const newBigPicComment = bigPictureComment.cloneNode(true);
-      newBigPicComment.querySelector(`.social__picture`).src = comment.avatar;
-      newBigPicComment.querySelector(`.social__picture`).alt = comment.name;
-      newBigPicComment.querySelector(`.social__text`).textContent = comment.message;
-      return newBigPicComment;
-    },
-
-    // Функцуия очистки комментариев для bigPicture
-    removeBigPicComments() {
-      bigPictureComments.innerHTML = ``;
-    },
-
-    // Функция отображения окна с полноэкранной фотографией
-    showBigPicture(currentImg) {
-      window.util.showModalWindow(bigPicture);
-      bigPictureImg.src = currentImg.url;
-      bigPictureLikesCount.textContent = currentImg.likes;
-      bigPictureCommentsCount.textContent = currentImg.comments.length;
-      bigPictureDescription.textContent = currentImg.description;
-      bigPicture.querySelector(`.social__comment-count`).classList.add(`hidden`);
-      bigPicture.querySelector(`.comments-loader`).classList.add(`hidden`);
-      window.bigPicture.insertBigPicComment(currentImg.comments);
-      // Обработчик закрытия окна по по нажатию Esc
-      document.addEventListener(`keydown`, window.bigPicture.onBigPictureEscPress);
-      // Обработчик закрытия окна по клику вне окна
-      bigPicture.addEventListener(`click`, window.bigPicture.bigPictureCloseHandler);
-      // Обработчик закрытия окна по кнопке "X"
-      bigPictureCloseBtn.addEventListener(`click`, window.bigPicture.onBigPictureCloseBtnPress);
-    },
-
-    bigPictureOpenHandler(evt) {
-      if (evt.target.closest(`img`) && window.photoEditor.classList.value.includes(`hidden`)) {
-        const pictureToShow = window.photosArr[evt.target.dataset.id];
-        window.bigPicture.showBigPicture(pictureToShow);
-      }
-    },
-
-    // Функция закрытия окна по кнопке Х
-    onBigPictureCloseBtnPress() {
-      window.bigPicture.closeBigPicture();
-    },
-
-    // Функция закрытия окна по нажатию Esc
-    onBigPictureEscPress(evt) {
-      if (evt.key === `Escape`) {
-        evt.preventDefault();
-        window.bigPicture.closeBigPicture();
-      }
-    },
-
-    // Функция закрытия окна по клику вне окна
-    bigPictureCloseHandler(evt) {
-      if (!evt.target.closest(`.big-picture__preview`)) {
-        window.bigPicture.closeBigPicture();
-      }
-    },
-
-    // Функция закрытия окна полноэкранной фотографии
-    closeBigPicture() {
-      window.util.hideModalWindow(bigPicture);
-      document.removeEventListener(`keydown`, window.bigPicture.onBigPictureEscPress);
-      bigPicture.removeEventListener(`click`, window.bigPicture.bigPictureCloseHandler);
-      bigPictureCloseBtn.removeEventListener(`click`, window.bigPicture.onBigPictureCloseBtnPress);
-    },
-
-    // Наполнение комментариев из массива для полноэкранного фото
-    insertBigPicComment(comments) {
-      window.bigPicture.removeBigPicComments();
-      const fragment = document.createDocumentFragment();
-      for (let i = 0; i < comments.length; i++) {
-        fragment.appendChild(window.bigPicture.getBigPicComment(comments[i]));
-      }
-      return bigPictureComments.appendChild(fragment);
-    },
+    show: showBigPicture,
   };
 })();
