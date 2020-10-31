5565200fceb131a9ce21879f95690591f4e0fbd3
bremxx <petr.kirienko@gmail.com>
2020-10-31 17:46:52 +0300

    фильтр случайных фото

--- js/main.js
+++ js/main.js
@@ -3,6 +3,30 @@
 
   const pictures = document.querySelector(`.pictures`);
   const photoUploader = document.querySelector(`.img-upload__input`);
+  let photos = [];
+  const imgFilters = document.querySelector(`.img-filters`);
+  const imgFiltersForm = imgFilters.querySelector(`.img-filters__form`);
+  const RANDOW_IMG_NUM = 10;
+
+  function showImgFilters() {
+    imgFilters.classList.remove(`img-filters--inactive`);
+    imgFiltersForm.addEventListener(`click`, filterChangeHandler);
+  }
+
+  function filterChangeHandler(evt) {
+    if (evt.target.id === `filter-random`) {
+      applyFilter();
+    }
+  }
+
+  function applyFilter() {
+    const pics = pictures.querySelectorAll(`.picture`);
+    for (let i = 0; i < pics.length; i++) {
+      let pic = pictures.querySelector(`.picture`);
+      pic.parentNode.removeChild(pic);
+    }
+    renderPhotos(window.util.shuffle(photos).slice(RANDOW_IMG_NUM * -1));
+  }
 
   function renderPhotos(imgs) {
     const fragment = document.createDocumentFragment();
@@ -17,7 +41,9 @@
   }
 
   function successLoadHandler(resp) {
-    renderPhotos(resp);
+    photos = resp;
+    renderPhotos(photos);
+    showImgFilters();
   }
 
   function errorLoadHandler(errorMessage) {
