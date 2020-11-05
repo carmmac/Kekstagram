f0eec4dc32ca5166f9e00d9f10ed370cdc9fbf5c
bremxx <petr.kirienko@gmail.com>
2020-11-05 17:36:13 +0300

    чистка кода в модуле form

--- js/form.js
+++ js/form.js
@@ -7,44 +7,57 @@
   const photoEditor = document.querySelector(`.img-upload__overlay`);
   const previewImg = photoEditor.querySelector(`.img-upload__preview img`);
   const photoEditorCloseBtn = photoEditor.querySelector(`.img-upload__cancel`);
-
   // ИЗМЕНЕНИЕ МАСШТАБА ИЗОБРАЖЕНИЯ
   const scalePanel = photoEditor.querySelector(`.img-upload__scale`);
   const scaleBtnSmaller = photoEditor.querySelector(`.scale__control--smaller`);
   const scaleValueField = photoEditor.querySelector(`.scale__control--value`);
   const INIT_SCALE_VALUE = 100;
   const SCALE_CHANGE_STEP = 25;
+  // ЭФФЕКТЫ ИЗОБРАЖЕНИЯ
+  const effectsPanel = photoEditor.querySelector(`.effects__list`);
+  const effectName = {
+    chrome: `grayscale`,
+    sepia: `sepia`,
+    marvin: `invert`,
+    phobos: `blur`,
+    heat: `brightness`,
+  };
+  // ИНТЕНСИВНОСТЬ ЭФФЕКТА
+  const effectLevelPanel = photoEditor.querySelector(`.img-upload__effect-level`);
+  const effectLevelBar = effectLevelPanel.querySelector(`.effect-level__line`);
+  const effectLevelPin = effectLevelPanel.querySelector(`.effect-level__pin`);
+  const effectLevelInput = effectLevelPanel.querySelector(`.effect-level__value`);
+  const effectLevelDepthBar = effectLevelPanel.querySelector(`.effect-level__depth`);
+  const INITIAL_EFFECT_LVL = 100;
+  // ВАЛИДАЦИЯ ХЭШТЕГОВ
+  const hashtagInput = photoEditor.querySelector(`.text__hashtags`);
+  const regExp = /^#[a-zA-Zа-яА-Я\d]+$/;
+  const MIN_HATSHTAG_LENGTH = 1;
+  const MAX_HATSHTAG_LENGTH = 20;
+  const MAX_HASHTAG_NUM = 5;
+  // ВАЛИДАЦИЯ КОММЕНТАРИЯ
+  const commentInput = photoEditor.querySelector(`.text__description`);
+  commentInput.maxLength = 140;
 
-  // Функция открытия окна редактора изображения
   function openEditor() {
     window.util.modal.show(photoEditor);
     if (getCurrentEffect() === null) {
       window.util.element.hide(effectLevelPanel);
     }
     scaleValueField.value = `${INIT_SCALE_VALUE}%`;
-    // Обработчик изменения масштаба
     scalePanel.addEventListener(`click`, scaleChangeHandler);
-    // Обработчик закрытия окна по кнопке "X"
     photoEditorCloseBtn.addEventListener(`click`, photoEditorCloseBtnPressHandler);
-    // Обработчик закрытия окна по по нажатию Esc
     document.addEventListener(`keydown`, photoEditorEscPressHandler);
-    // Обработчик переключения эффектов на изображении
     effectsPanel.addEventListener(`change`, effectChangeHandler);
-    // Обработчик уровня эффекта
     effectLevelPin.addEventListener(`mousedown`, effectLevelChangeHandler);
-    // Обработчик ввода хэштегов
     hashtagInput.addEventListener(`input`, checkHashtagValidity);
-    // Обработчики фокусировки на поле комментария
     commentInput.addEventListener(`focusin`, preventEscPress);
     commentInput.addEventListener(`focusout`, restoreEscPress);
-    // Обработчики фокусировки на поле хэштегов
     hashtagInput.addEventListener(`focusin`, preventEscPress);
     hashtagInput.addEventListener(`focusout`, restoreEscPress);
-    // Обработчик отправки изображения
     photoUploadForm.addEventListener(`submit`, successPostHandler);
   }
 
-  // Функция закрытия редактора изображения
   function closePhotoEditor() {
     photoUploader.value = ``;
     previewImg.style.transform = ``;
@@ -67,12 +80,10 @@
     photoUploadForm.removeEventListener(`submit`, successPostHandler);
   }
 
-  // Функция закрытия редактора по кнопке Х
   function photoEditorCloseBtnPressHandler() {
     closePhotoEditor();
   }
 
-  // Функция закрытия редактора по нажатию Esc
   function photoEditorEscPressHandler(evt) {
     if (evt.key === `Escape`) {
       evt.preventDefault();
@@ -95,7 +106,6 @@
     evt.preventDefault();
   }
 
-  // Функция изменения масштаба превью-изображения
   function changeImgScale(value) {
     previewImg.style.transform = `scale(${value / 100})`;
   }
@@ -108,7 +118,6 @@
     return currScale === 100 ? currScale : currScale + SCALE_CHANGE_STEP;
   }
 
-  // Функция применения масштаба
   function scaleChangeHandler(evt) {
     const currentScale = parseInt(scaleValueField.value, 10);
     let newScale;
@@ -121,17 +130,6 @@
     changeImgScale(newScale);
   }
 
-
-  // ЭФФЕКТЫ ИЗОБРАЖЕНИЯ
-  const effectsPanel = photoEditor.querySelector(`.effects__list`);
-  const effectName = {
-    chrome: `grayscale`,
-    sepia: `sepia`,
-    marvin: `invert`,
-    phobos: `blur`,
-    heat: `brightness`,
-  };
-
   function effectChangeHandler(evt) {
     if (evt.target.matches(`input[type="radio"]`)) {
       const currentEffectName = evt.target.value;
@@ -140,7 +138,6 @@
     }
   }
 
-  // Функция применения эффектов
   function changeEffect(value) {
     const currentEffect = getCurrentEffect();
     if (currentEffect !== `effects__preview--${value}`) {
@@ -155,19 +152,16 @@
     }
   }
 
-  // Функция добавления эффекта
   function addEffect(effect) {
     previewImg.classList.add(`effects__preview--${effect}`);
   }
 
-  // Функция удаления эффекта
   function removeEffect(effectClass) {
     previewImg.classList.remove(effectClass);
     previewImg.style.filter = ``;
     effectLevelDepthBar.style.width = `${INITIAL_EFFECT_LVL}%`;
   }
 
-  // Функция проверки наличия эффекта
   function getCurrentEffect() {
     const classes = previewImg.classList;
     for (let i = 0; i < classes.length; i++) {
@@ -179,17 +173,6 @@
     return null;
   }
 
-
-  // ИНТЕНСИВНОСТЬ ЭФФЕКТА
-  // начальная реализация по заданию
-  const effectLevelPanel = photoEditor.querySelector(`.img-upload__effect-level`);
-  const effectLevelBar = effectLevelPanel.querySelector(`.effect-level__line`);
-  const effectLevelPin = effectLevelPanel.querySelector(`.effect-level__pin`);
-  const effectLevelInput = effectLevelPanel.querySelector(`.effect-level__value`);
-  const effectLevelDepthBar = effectLevelPanel.querySelector(`.effect-level__depth`);
-  const INITIAL_EFFECT_LVL = 100;
-
-  // Функция изменения глубины эффекта
   function effectLevelChangeHandler(evt) {
     evt.preventDefault();
     const maxEffectLevel = effectLevelBar.offsetWidth;
@@ -247,14 +230,6 @@
     }
   }
 
-  // ВАЛИДАЦИЯ ХЭШТЕГОВ
-  const hashtagInput = photoEditor.querySelector(`.text__hashtags`);
-  const regExp = /^#[a-zA-Zа-яА-Я\d]+$/;
-  const MIN_HATSHTAG_LENGTH = 1;
-  const MAX_HATSHTAG_LENGTH = 20;
-  const MAX_HASHTAG_NUM = 5;
-
-  // Функция проверки валижности хэштега
   function checkHashtagValidity(evt) {
     let hashtags = evt.target.value.split(/ +/);
     for (let i = 0; i < hashtags.length; i++) {
@@ -275,12 +250,10 @@
     }
   }
 
-  // Функция проверки одиковых тэгов
   function checkIdenticalHashtags(arr) {
     return arr.some((item) => arr.indexOf(item) !== arr.lastIndexOf(item));
   }
 
-  // Функция проверки пустых тэгов
   function checkEmptyHashtag(arr) {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i].length === MIN_HATSHTAG_LENGTH) {
@@ -289,11 +262,6 @@
     }
     return false;
   }
-
-
-  // ВАЛИДАЦИЯ КОММЕНТАРИЯ
-  const commentInput = photoEditor.querySelector(`.text__description`);
-  commentInput.maxLength = 140;
 
   function preventEscPress() {
     document.removeEventListener(`keydown`, photoEditorEscPressHandler);
