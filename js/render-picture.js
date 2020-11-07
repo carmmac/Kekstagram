3a3103b813ab44805636caf3f8bf645d68d47b1c
bremxx <petr.kirienko@gmail.com>
2020-11-07 18:04:00 +0300

    удаление лишних переменных с темплейтами

--- js/render-picture.js
+++ js/render-picture.js
@@ -1,12 +1,11 @@
 "use strict";
 
-const pictureTemplate = document.querySelector(`#picture`);
-const pictureTemplateContent = pictureTemplate.content.querySelector(
+const pictureTemplate = document.querySelector(`#picture`).content.querySelector(
     `.picture`
 );
 
 window.renderPicture = function (photo) {
-  const newPicture = pictureTemplateContent.cloneNode(true);
+  const newPicture = pictureTemplate.cloneNode(true);
   const newPictureImg = newPicture.querySelector(`.picture__img`);
   newPictureImg.src = photo.url;
   newPicture.querySelector(`.picture__likes`).textContent = photo.likes;
